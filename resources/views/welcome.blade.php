502186c560f8d1b03e5ef800d36b6b7da521b66a
Gabriel Godoy <gabyreload@gmail.com>
2025-09-01 22:44:28 -0300

    init tuto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ninja Network</title>
+</head>
+
+<body>
+    <h1>Ninja Network</h1>
+    <a href="/ninjas">Ninjas</a>
+</body>
+
+</html>
