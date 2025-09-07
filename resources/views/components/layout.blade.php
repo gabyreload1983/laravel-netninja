10f41a3ffe8820690d10132635ab816812282b9a
Gabriel Godoy <gabyreload@gmail.com>
2025-09-07 12:05:36 -0300

    css and taildwind

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,14 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ninja Network</title>
+
+    @vite('resources/css/app.css')
 </head>
 
 <body>
+
     <header>
         <nav>
-            <h1>Ninjas</h1>
-            <a href="/ninjas">All Ninjas</a>
-            <a href="/ninjas/create">New Ninja</a>
+            <h1>Ninja Network</h1>
+            <a href="/ninjas">
+                All Ninjas
+            </a>
+            <a href="/ninjas/create">
+                Create New Ninja
+            </a>
         </nav>
     </header>
 
