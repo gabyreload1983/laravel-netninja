70e7045b81cbf7ffe4c59a108613d8d21a5bf526
Gabriel Godoy <gabyreload@gmail.com>
2025-09-11 19:21:20 -0300

    foreing key controller

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -4,7 +4,10 @@
         @foreach($ninjas as $ninja)
         <li>
             <x-card href="{{ route('ninjas.show', $ninja->id) }}" :highlight="$ninja['skill'] > 80">
-                <h3>{{ $ninja['name'] }}</h3>
+                <div>
+                    <h3>{{ $ninja['name'] }}</h3>
+                    <p>{{ $ninja->dojo->name }}</p>
+                </div>
             </x-card>
         </li>
         @endforeach
